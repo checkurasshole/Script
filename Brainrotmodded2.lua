ab29095992cc9167c163aa6637570d48f0fbcc4d
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-06-20 12:01:09 -0500

    Update Brainrotmodded2.lua

--- Brainrotmodded2.lua
+++ Brainrotmodded2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/checkurasshole/f94efb33c1a14600fc073fdc93d0c842/raw/a48f6bdeae9ae96201591e9f0d4865ba77a18fa1/Brainrotmodded.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/checkurasshole/f94efb33c1a14600fc073fdc93d0c842/raw/93bf1a48446c43c215b7545936a8d17caf9ee87e/Brainrotmodded.lua"))()
