5989a3397eea71c5d9034e8094be9c53749c05be
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-06-27 07:16:13 -0500

    Update AnimeInfmoney.lua

--- AnimeInfmoney.lua
+++ AnimeInfmoney.lua
@@ -1,5 +1,5 @@
 loadstring(
     game:HttpGet(
-        'https://v0-supabase-secure-storage.vercel.app/api/script/0131e2c3e90b034169026b3615de3507'
+        'https://v0-supabase-secure-storage.vercel.app/api/script/0d4a06a2d430f55dd68216bb3e0f658e'
     )
 )()
