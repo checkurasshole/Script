7a796e74c391d11b8a6a0d5504572db1379f94db
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-09-11 07:58:21 -0500

    🔄 Migrate script: My Test Script

---
+++ vault/74013c59eb2b92690fae0a9b38c84392.lua
@@ -0,0 +1,9 @@
+-- My Test Script
+-- Script ID: 74013c59eb2b92690fae0a9b38c84392
+-- Migrated: 2025-09-11T12:58:21.178Z
+-- Auto-migrated from encrypted storage to GitHub
+
+-- Example Lua script with external URL
+loadstring(game:HttpGet("__URL_3bc277371ac0b804__"))()
+
+print("This script will be protected offline!")
