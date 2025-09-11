2042bc31d5d9ef9ee844cf433001a0cb35f276b3
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-09-11 09:25:29 -0500

    🔄 Migrate script: Premium Script

---
+++ vault/c52c8d40aa177bd293cc55ec46ff4be3.lua
@@ -0,0 +1,9 @@
+-- Premium Script
+-- Script ID: c52c8d40aa177bd293cc55ec46ff4be3
+-- Migrated: 2025-09-11T14:25:29.514Z
+-- Auto-migrated from encrypted storage to GitHub
+
+
+-- My Website's Premium Script v1.0
+print("Premium features unlocked!")
+loadstring(game:HttpGet("__URL_41bee284cad3cb06__"))()
