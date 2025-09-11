9b322239766908a1a69d5164379ab1aea687bd23
checkurasshole <86085809+checkurasshole@users.noreply.github.com>
2025-09-11 09:25:28 -0500

    🔄 Migrate script: Main Script

---
+++ vault/8479babacca93ab63ab315aa7393609a.lua
@@ -0,0 +1,9 @@
+-- Main Script
+-- Script ID: 8479babacca93ab63ab315aa7393609a
+-- Migrated: 2025-09-11T14:25:28.710Z
+-- Auto-migrated from encrypted storage to GitHub
+
+
+-- My Website's Main Script v1.0
+print("Welcome to my website!")
+loadstring(game:HttpGet("__URL_b14fdce6abcde2dc__"))()
